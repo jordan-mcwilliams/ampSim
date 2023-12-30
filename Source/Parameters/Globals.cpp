f539fd9b38d30f7e732d32a470ede99473245933
jordan-mcwilliams <jsmcwilliams@gmail.com>
2023-12-30 13:56:53 -0800

    Added parameters? Not sure I need?
    It was in tutorial but these files are throwing errors, might delete / revert commits.

---
+++ Source/Parameters/Globals.cpp
@@ -0,0 +1,15 @@
+#include "Globals.h"
+
+// Audio Parameters
+
+// extern const juce::String presetID = "presets";
+// extern const juce::String presetName = "Presets";
+
+const juce::String inpurtID = "input";
+const juce::String inputName = "Input";
+
+const juce::String outputID = "output";
+const juce::String outputName = "Output";
+
+const juce::String hqID = "hq";
+const juce::String hqName = "High Quality";
---
+++ Source/Parameters/Globals.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <JuceHeader.h>
+
+// Audio Parameters
+
+// extern const juce::String presetID = "presets";
+// extern const juce::String presetName = "Presets";
+
+extern const juce::String inpurtID = "input";
+extern const juce::String inputName = "Input";
+
+extern const juce::String outputID = "output";
+extern const juce::String outputName = "Output";
+
+extern const juce::String hqID = "hq";
+extern const juce::String hqName = "High Quality";
